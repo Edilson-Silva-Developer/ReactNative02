85c6f9307aed580a62ba8bd1f99a85de7cc83376
EdilsonSilva <edilsonantonio0@gmail.com>
2023-06-08 22:23:52 -0300

    ok

---
+++ JavaScript-Estudos/116-encontrandoelementos/tagName.js
@@ -0,0 +1,11 @@
+//acessando elementos copm a tagName
+
+console.log(document.getElementsByTagName('h1'));
+
+console.log(document.getElementsByTagName('p'));
+
+console.log(document.getElementsByTagName('li'));
+
+console.log(document.getElementsByTagName('h2'));
+
+console.log(document.getElementsByTagName('div'));
