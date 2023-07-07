0d50e760ee73ae83122ef85ebc020854b836612c
EdilsonSilva <edilsonantonio0@gmail.com>
2023-07-06 22:22:03 -0300

    ok

---
+++ JavaScript-Estudos/124-posicaoDeElementoNaTela/posicaoDoElementoNaTela.js
@@ -0,0 +1,9 @@
+let elemento =  document.querySelector('#titulo-principal');
+
+console.log(elemento.getBoundingClientRect());
+
+
+
+let elemento2 = document.querySelector('#Segundo-texto');
+
+console.log(elemento.getBoundingClientRect());
---
+++ JavaScript-Estudos/126-selecionarVariosElementosComQuery/selecionarVariosElemntosComQuery.js
@@ -0,0 +1,8 @@
+let itens = document.querySelectorAll('.itens-verde');
+
+
+console.log(itens[0].style.color)
+
+itens[0].style.color = 'red';
+
+console.log(itens[0].style.color);
